a2a16b59ef4687cd1beca8ee3e3dbf8f5ef6e328
Arif Nugroho <37043297+arif2705@users.noreply.github.com>
2019-03-09 14:38:04 +0700

    ok

---
+++ program serial/program serial/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace program_serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSerial = new System.Windows.Forms.Label();
+            this.lblBaudrate = new System.Windows.Forms.Label();
+            this.cbbSerial = new System.Windows.Forms.ComboBox();
+            this.cbbBaudrate = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.rctSend = new System.Windows.Forms.RichTextBox();
+            this.rctReceive = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSerial
+            // 
+            this.lblSerial.AutoSize = true;
+            this.lblSerial.Location = new System.Drawing.Point(12, 54);
+            this.lblSerial.Name = "lblSerial";
+            this.lblSerial.Size = new System.Drawing.Size(55, 13);
+            this.lblSerial.TabIndex = 0;
+            this.lblSerial.Text = "Serial Port";
+            // 
+            // lblBaudrate
+            // 
+            this.lblBaudrate.AutoSize = true;
+            this.lblBaudrate.Location = new System.Drawing.Point(12, 80);
+            this.lblBaudrate.Name = "lblBaudrate";
+            this.lblBaudrate.Size = new System.Drawing.Size(50, 13);
+            this.lblBaudrate.TabIndex = 1;
+            this.lblBaudrate.Text = "Baudrate";
+            // 
+            // cbbSerial
+            // 
+            this.cbbSerial.FormattingEnabled = true;
+            this.cbbSerial.Location = new System.Drawing.Point(82, 51);
+            this.cbbSerial.Name = "cbbSerial";
+            this.cbbSerial.Size = new System.Drawing.Size(121, 21);
+            this.cbbSerial.TabIndex = 2;
+            // 
+            // cbbBaudrate
+            // 
+            this.cbbBaudrate.FormattingEnabled = true;
+            this.cbbBaudrate.Location = new System.Drawing.Point(82, 78);
+            this.cbbBaudrate.Name = "cbbBaudrate";
+            this.cbbBaudrate.Size = new System.Drawing.Size(121, 21);
+            this.cbbBaudrate.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(15, 115);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(85, 29);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(115, 114);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(88, 31);
+            this.btnDisconnect.TabIndex = 5;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMessage.Location = new System.Drawing.Point(15, 158);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(188, 22);
+            this.lblMessage.TabIndex = 8;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(15, 194);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(188, 20);
+            this.txtSend.TabIndex = 9;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(15, 229);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(188, 45);
+            this.btnSend.TabIndex = 10;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // rctSend
+            // 
+            this.rctSend.Location = new System.Drawing.Point(235, 51);
+            this.rctSend.Name = "rctSend";
+            this.rctSend.Size = new System.Drawing.Size(236, 223);
+            this.rctSend.TabIndex = 11;
+            this.rctSend.Text = "";
+            // 
+            // rctReceive
+            // 
+            this.rctReceive.Location = new System.Drawing.Point(489, 51);
+            this.rctReceive.Name = "rctReceive";
+            this.rctReceive.Size = new System.Drawing.Size(236, 223);
+            this.rctReceive.TabIndex = 12;
+            this.rctReceive.Text = "";
+            this.rctReceive.TextChanged += new System.EventHandler(this.rctReceive_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(327, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Data Send";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(570, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Data Receive";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(222, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(300, 24);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Serial Communication Interface";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(745, 297);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.rctReceive);
+            this.Controls.Add(this.rctSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cbbBaudrate);
+            this.Controls.Add(this.cbbSerial);
+            this.Controls.Add(this.lblBaudrate);
+            this.Controls.Add(this.lblSerial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSerial;
+        private System.Windows.Forms.Label lblBaudrate;
+        private System.Windows.Forms.ComboBox cbbSerial;
+        private System.Windows.Forms.ComboBox cbbBaudrate;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.RichTextBox rctSend;
+        private System.Windows.Forms.RichTextBox rctReceive;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
